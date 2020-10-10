e5bbd38bbd98f16af091c214a94f8792564d4b4d
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2020-10-09 18:08:41 -0700

    aysnc await

--- src/search.js
+++ src/search.js
@@ -2,48 +2,51 @@ import React, { useState, useEffect } from 'react';
 import { Table } from './table';
 
 export function SearchBar() {
-  const [status, setStatus] = useState('idle');
   const [data, setData] = useState();
   const [doNotMutate, setOriginal] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      setStatus('fetching');
-      await fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(data => {
-          setData(data)
-          setOriginal(data)
-        })
-        .then(() => setStatus('fetched'))
-        .catch(error => alert(error));
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        const data = await response.json();
+        setData(data);
+        setOriginal(data);
+      } catch (err) {
+        alert(err);
+      }
     };
+
     fetchData();
   }, []);
 
-  const search = (event) => {
+  const search = event => {
     event.preventDefault();
-   
-    const filtered = data.filter((el)=> {
-      if(!isNaN(event.target.value)) {
-        return el.phone.toLowerCase().includes(event.target.value.toLowerCase());
+
+    const filtered = data.filter(el => {
+      if (!isNaN(event.target.value)) {
+        return el.phone
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase());
       } else {
         return el.name.toLowerCase().includes(event.target.value.toLowerCase());
       }
-    })
+    });
 
-   if(event.target.value === '') {
-    setData(doNotMutate)
-   } else {
-    setData(filtered);
-   }
-  }
+    if (event.target.value === '') {
+      setData(doNotMutate);
+    } else {
+      setData(filtered);
+    }
+  };
 
   return (
     <div>
       <div className='search-label'>Search</div>
       <input onChange={search} type='input' className='search-input'></input>
-      <Table data={data}/>
+      <Table data={data} />
     </div>
   );
 }
