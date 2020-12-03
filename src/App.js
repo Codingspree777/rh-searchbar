276ff99c71c533eb754e3a813ddce1486395d107
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2020-12-03 14:09:49 -0800

    reusable component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SearchBar } from './search';
 import './App.css';
 
 function App() {
+  const [searchData, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      // const fetchData = async () => {
+      //   try {
+      //     const response = await fetch(
+      //       'https://jsonplaceholder.typicode.com/users'
+      //     );
+      //     const data = await response.json();
+      //     setData(data);
+      //     setOriginal(data);
+      //   } catch (err) {
+      //     alert(err);
+      //   }
+      // };
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(err => alert(err));
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <div className="Search-Container">
-      <SearchBar/>
+    <div className='App'>
+      <div className='Search-Container'>
+        <SearchBar searchData={searchData} />
       </div>
     </div>
   );
--- src/search.js
+++ src/search.js
@@ -1,42 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Table } from './table';
 
-export function SearchBar() {
+export function SearchBar({ searchData }) {
   const [data, setData] = useState([]);
-  const [doNotMutate, setOriginal] = useState([]);
-
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/users'
-        );
-        const data = await response.json();
-        setData(data);
-        setOriginal(data);
-      } catch (err) {
-        alert(err);
-      }
-    };
-
-    fetchData();
-  }, []);
+    setData(searchData);
+  }, [searchData]);
 
   const search = event => {
     let text = event.target.value;
+
     const filtered = data.filter(el => {
       if (!isNaN(text)) {
-        return el.phone
-          .toLowerCase()
-          .includes(text.toLowerCase());
+        return el.phone.toLowerCase().includes(text.toLowerCase());
       } else {
         return el.name.toLowerCase().includes(text.toLowerCase());
       }
     });
 
     if (text === '') {
-      setData(doNotMutate);
+      setData(searchData);
     } else {
       setData(filtered);
     }
